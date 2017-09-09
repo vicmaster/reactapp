41dc6d678bb7b25d982f077273c9633468e1cefe
Victor Velazquez <velazquezgaspar16@hotmail.com>
2017-09-09 14:22:39 -0500

    Learning react components
    * Add profile section (create a component)

---
+++ src/components/Profile/Profile.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class Profile extends Component {
+
+  render() {
+    return (
+      <section>
+        <img src="https://avatars2.githubusercontent.com/u/969187?v=4&s=460" width="200px"/>
+        <p>Victor Velazquez</p>
+        <p>velazquezgaspar16@gmail.com</p>
+      </section>
+    );
+  }
+};
+
+export default Profile;
---
+++ src/components/Profile/index.js
@@ -0,0 +1,13 @@
+import Profile from "./Profile";
+// import ProfileActions from "./ProfileActions";
+// import ProfileContainer from "./ProfileContainer";
+// import ProfileReducer from "./ProfileReducer";
+
+// export {
+  // Profile
+  // ProfileActions,
+  // ProfileContainer,
+  // ProfileReducer
+// }
+
+export default Profile;
