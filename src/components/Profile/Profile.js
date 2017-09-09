7766a5128e3c5c53fdb990bbb6b9b2676bf80fdd
Victor Velazquez <velazquezgaspar16@hotmail.com>
2017-09-09 15:04:23 -0500

    Update the component state

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -9,6 +9,22 @@ class Profile extends Component {
       email: "velazquezgaspar16@gmail.com",
       avatar: "https://avatars2.githubusercontent.com/u/969187?v=4&s=460"
     }
+
+    this.onChangeName = this.onChangeName.bind(this);
+    this.onChangeEmail = this.onChangeEmail.bind(this);
+    this.onChangeAvatar = this.onChangeAvatar.bind(this);
+  }
+
+  onChangeName({target: { value }}) {
+    this.setState({ name: value });
+  }
+
+  onChangeEmail({target: { value }}) {
+    this.setState({ email: value });
+  }
+
+  onChangeAvatar({target: { value }}) {
+    this.setState({ avatar: value });
   }
 
   render() {
@@ -17,6 +33,9 @@ class Profile extends Component {
         <img src={ this.state.avatar }   width="200px"/>
         <p>{ this.state.name }</p>
         <p>{ this.state.email }</p>
+        <input type="text" name="name" onChange={ this.onChangeName } value={ this.state.name }/>
+        <input type="email" name="email" onChange={ this.onChangeEmail } value={ this.state.email }/>
+        <input type="text" name="avatar" onChange={ this.onChangeAvatar }  value={ this.state.avatar}/>
       </section>
     );
   }
